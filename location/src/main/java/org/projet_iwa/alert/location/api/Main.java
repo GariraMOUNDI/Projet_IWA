7f6e82726fe260d3a364eeec680f56f3de202865
Garira MOUNDI <mazougarira@yahoo.fr>
2020-10-23 21:50:13 +0200

    Gradlew build fixed

--- location/src/main/java/org/projet_iwa/alert/location/api/Main.java
+++ location/src/main/java/org/projet_iwa/alert/location/api/Main.java
@@ -1,11 +1,11 @@
 package org.projet_iwa.alert.location.api;
 
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication(scanBasePackages = "org.projet_iwa")
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
+//
+//@SpringBootApplication(scanBasePackages = "org.projet_iwa")
 public class Main {
 
-    public static void main(String[] args) {
-        System.out.println("Welcome to Location Microservice");
-    }
+//    public static void main(String[] args) {
+//        System.out.println("Welcome to Location Microservice");
+//    }
 }
