ddded67a77f5cb5017058c3c1bd47597ed13208a
Garira MOUNDI <mazougarira@yahoo.fr>
2020-11-14 16:34:39 +0100

    Alert Ok

--- auth/src/main/java/org/projet_iwa/auth/api/repository/VerificationTokenRepository.java
+++
@@ -1,8 +0,0 @@
-package org.projet_iwa.auth.api.repository;
-
-import org.projet_iwa.auth.api.model.VerificationToken;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface VerificationTokenRepository extends JpaRepository<VerificationToken, String> { }
