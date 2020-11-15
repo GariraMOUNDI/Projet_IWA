835c462f3da9f6152ebf724eef579a2e0a6fe9e4
Garira MOUNDI <mazougarira@yahoo.fr>
2020-11-15 16:43:56 +0100

    Create user test Ok

--- auth/src/main/java/org/projet_iwa/auth/api/repository/VerificationTokenRepository.java
+++ auth/src/main/java/org/projet_iwa/auth/api/repository/VerificationTokenRepository.java
@@ -4,5 +4,7 @@ import org.projet_iwa.auth.api.model.VerificationToken;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface VerificationTokenRepository extends JpaRepository<VerificationToken, String> { }
