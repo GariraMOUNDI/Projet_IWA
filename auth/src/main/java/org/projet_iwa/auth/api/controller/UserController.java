01477b0a19e71aab65c38d8fac887a7502175b9d
Garira MOUNDI <mazougarira@yahoo.fr>
2020-11-18 14:37:11 +0100

    Cross origin

--- auth/src/main/java/org/projet_iwa/auth/api/controller/UserController.java
+++ auth/src/main/java/org/projet_iwa/auth/api/controller/UserController.java
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.*;
 import javax.annotation.security.RolesAllowed;
 import java.util.List;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/users")
 public class UserController {
