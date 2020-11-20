111a7c76b3fb34abbacb612b3c10f00abce1173f
Garira MOUNDI <mazougarira@yahoo.fr>
2020-11-21 00:25:08 +0100

    Test skipped for deployment

--- auth/src/test/java/org/projet_iwa/auth/api/UserControllerTest.java
+++ auth/src/test/java/org/projet_iwa/auth/api/UserControllerTest.java
@@ -1,88 +1,88 @@
-//package org.projet_iwa.auth.api;
-//
-//import org.junit.jupiter.api.Test;
-//import org.projet_iwa.auth.api.model.UserResponse;
-//import org.projet_iwa.auth.api.model.UserResponseType;
-//import org.projet_iwa.auth.api.service.IUserService;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-//import org.springframework.boot.test.context.SpringBootTest;
-//import org.springframework.boot.test.mock.mockito.MockBean;
-//import org.springframework.http.MediaType;
-//import org.springframework.test.web.servlet.MockMvc;
-//
-//import static org.mockito.ArgumentMatchers.any;
-//import static org.mockito.Mockito.doReturn;
-//import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-//import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-//
-//@AutoConfigureMockMvc
-//@SpringBootTest
-//public class UserControllerTest {
-//
-//    @Autowired
-//    private MockMvc mockMvc;
-//
-//    @MockBean
-//    private IUserService iUserService;
-//
-//    @Test
-//    public void createUser() throws Exception {
-//        doReturn(new UserResponse(UserResponseType.USER_CREATED))
-//                .when(iUserService).createUser(any());
-//
-//        mockMvc.perform(
-//                post("/users/create")
-//                    .contentType(MediaType.APPLICATION_JSON)
-//                    .content(SampleData.toJson(SampleData.getSampleDTO()))
-//        ).andExpect(status().isCreated())
-//        .andExpect(content().contentType(MediaType.APPLICATION_JSON))
-//        .andExpect(jsonPath("$.type").value(UserResponseType.USER_CREATED.toString()))
-//        .andExpect(jsonPath("$.payload").isEmpty());
-//
-//    }
-//
-//    @Test
-//    public void loginUser() throws Exception {
-//        doReturn(new UserResponse(UserResponseType.USER_LOGGED_IN, SampleData.getSampleDTO()))
-//                .when(iUserService).loginUser(any(),any());
-//
-//        mockMvc.perform(
-//                post("/users/login")
-//                        .contentType(MediaType.APPLICATION_JSON)
-//                        .content(SampleData.toJson(SampleData.getSampleDTO()))
-//        ).andExpect(status().isOk())
-//        .andExpect(content().contentType(MediaType.APPLICATION_JSON))
-//        .andExpect(jsonPath("$.type").value(UserResponseType.USER_LOGGED_IN.toString()))
-//        .andExpect(jsonPath("$.payload").isNotEmpty())
-//        .andExpect(jsonPath("$.payload.token.access_token").value(SampleData.getSampleDTO().getToken().getToken()));
-//    }
-//
-//    @Test
-//    public void confirmUser() throws Exception {
-//        doReturn(new UserResponse(UserResponseType.USER_CONFIRMED))
-//                .when(iUserService).confirmUser(any());
-//
-//        mockMvc.perform(
-//                get("/users/confirmUser?token=qsjdlkqshkdhqsdlkqsjlksqflkhq")
-//            ).andExpect(status().isOk())
-//            .andExpect(content().contentType(MediaType.APPLICATION_JSON))
-//            .andExpect(jsonPath("$.type").value(UserResponseType.USER_CONFIRMED.toString()))
-//            .andExpect(jsonPath("$.payload").isEmpty());
-//    }
-//
-//    @Test
-//    public void forgotUser() throws Exception {
-//        doReturn(new UserResponse(UserResponseType.FORGOT_EMAIL))
-//                .when(iUserService).forgotPassword(any());
-//
-//        mockMvc.perform(
-//                post("/users/forgotUser")
-//                        .contentType(MediaType.APPLICATION_JSON)
-//                        .content(SampleData.toJson(SampleData.getSampleDTO()))
-//        ).andExpect(status().isOk())
-//        .andExpect(content().contentType(MediaType.APPLICATION_JSON))
-//        .andExpect(jsonPath("$.type").value(UserResponseType.FORGOT_EMAIL.toString()))
-//        .andExpect(jsonPath("$.payload").isEmpty());
-//    }
-//}
+package org.projet_iwa.auth.api;
+
+import org.junit.jupiter.api.Test;
+import org.projet_iwa.auth.api.model.UserResponse;
+import org.projet_iwa.auth.api.model.UserResponseType;
+import org.projet_iwa.auth.api.service.IUserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doReturn;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@AutoConfigureMockMvc
+@SpringBootTest
+public class UserControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private IUserService iUserService;
+
+    @Test
+    public void createUser() throws Exception {
+        doReturn(new UserResponse(UserResponseType.USER_CREATED))
+                .when(iUserService).createUser(any());
+
+        mockMvc.perform(
+                post("/users/create")
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .content(SampleData.toJson(SampleData.getSampleDTO()))
+        ).andExpect(status().isCreated())
+        .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+        .andExpect(jsonPath("$.type").value(UserResponseType.USER_CREATED.toString()))
+        .andExpect(jsonPath("$.payload").isEmpty());
+
+    }
+
+    @Test
+    public void loginUser() throws Exception {
+        doReturn(new UserResponse(UserResponseType.USER_LOGGED_IN, SampleData.getSampleDTO()))
+                .when(iUserService).loginUser(any(),any());
+
+        mockMvc.perform(
+                post("/users/login")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(SampleData.toJson(SampleData.getSampleDTO()))
+        ).andExpect(status().isOk())
+        .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+        .andExpect(jsonPath("$.type").value(UserResponseType.USER_LOGGED_IN.toString()))
+        .andExpect(jsonPath("$.payload").isNotEmpty())
+        .andExpect(jsonPath("$.payload.token.access_token").value(SampleData.getSampleDTO().getToken().getToken()));
+    }
+
+    @Test
+    public void confirmUser() throws Exception {
+        doReturn(new UserResponse(UserResponseType.USER_CONFIRMED))
+                .when(iUserService).confirmUser(any());
+
+        mockMvc.perform(
+                get("/users/confirmUser?token=qsjdlkqshkdhqsdlkqsjlksqflkhq")
+            ).andExpect(status().isOk())
+            .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+            .andExpect(jsonPath("$.type").value(UserResponseType.USER_CONFIRMED.toString()))
+            .andExpect(jsonPath("$.payload").isEmpty());
+    }
+
+    @Test
+    public void forgotUser() throws Exception {
+        doReturn(new UserResponse(UserResponseType.FORGOT_EMAIL))
+                .when(iUserService).forgotPassword(any());
+
+        mockMvc.perform(
+                post("/users/forgotUser")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(SampleData.toJson(SampleData.getSampleDTO()))
+        ).andExpect(status().isOk())
+        .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+        .andExpect(jsonPath("$.type").value(UserResponseType.FORGOT_EMAIL.toString()))
+        .andExpect(jsonPath("$.payload").isEmpty());
+    }
+}
--- auth/src/test/java/org/projet_iwa/auth/api/UserServiceTest.java
+++ auth/src/test/java/org/projet_iwa/auth/api/UserServiceTest.java
@@ -1,75 +1,75 @@
-//package org.projet_iwa.auth.api;
-//
-//import org.junit.jupiter.api.DisplayName;
-//import org.junit.jupiter.api.Test;
-//import org.projet_iwa.auth.api.config.KeycloakService;
-//import org.projet_iwa.auth.api.model.*;
-//import org.projet_iwa.auth.api.repository.UserRepository;
-//import org.projet_iwa.auth.api.repository.VerificationTokenRepository;
-//import org.projet_iwa.auth.api.service.MailUtil;
-//import org.projet_iwa.auth.api.service.UserService;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-//import org.springframework.boot.test.context.SpringBootTest;
-//import org.springframework.boot.test.mock.mockito.MockBean;
-//
-//import java.util.Optional;
-//
-//import static  org.assertj.core.api.Assertions.*;
-//import static org.mockito.ArgumentMatchers.any;
-//import static org.mockito.Mockito.doReturn;
-//
-//@AutoConfigureMockMvc
-//@SpringBootTest
-//public class UserServiceTest {
-//
-//    @Autowired
-//    private UserService userService;
-//
-//    @Autowired
-//    private UserFactory userFactory;
-//
-//    @MockBean
-//    private UserRepository userRepository;
-//
-//    @MockBean
-//    private VerificationTokenRepository verificationTokenRepository;
-//
-//    @MockBean
-//    private KeycloakService keycloakService;
-//
-//    @MockBean
-//    private MailUtil mailUtil;
-//
-//    @Test
-//    @DisplayName("Create user without error")
-//    public void createUser(){
-//        doReturn(false)
-//                .when(userRepository).existsByUsername(any());
-//        doReturn(false)
-//                .when(userRepository).existsByEmail(any());
-//        doReturn(null)
-//                .when(userRepository).saveAndFlush(any());
-//        doReturn(null)
-//                .when(verificationTokenRepository).saveAndFlush(any());
-//
-//        UserResponse response = userService.createUser(SampleData.getSampleDTO());
-//
-//        assertThat(UserResponseType.USER_CREATED).isEqualTo(response.getType());
-//    }
-//
-//    @Test
-//    @DisplayName("Login user without error")
-//    public void loginUser() {
-//        doReturn(Optional.of(SampleData.getSampleUser()))
-//                .when(userRepository).findByUsername(any());
-//        doReturn(SampleData.getSampleDTO().getToken())
-//                .when(keycloakService).getToken(any(), any());
-//
-//        UserResponse response = userService.loginUser(SampleData.getSampleDTO().getUsername(), SampleData.getSampleDTO().getPassword());
-//
-//        assertThat(response.getType()).isEqualTo(UserResponseType.USER_LOGGED_IN);
-//        assertThat(response.getPayload().getToken().getToken()).isEqualTo(SampleData.getSampleDTO().getToken().getToken());
-//    }
-//
-//}
+package org.projet_iwa.auth.api;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.projet_iwa.auth.api.config.KeycloakService;
+import org.projet_iwa.auth.api.model.*;
+import org.projet_iwa.auth.api.repository.UserRepository;
+import org.projet_iwa.auth.api.repository.VerificationTokenRepository;
+import org.projet_iwa.auth.api.service.MailUtil;
+import org.projet_iwa.auth.api.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import java.util.Optional;
+
+import static  org.assertj.core.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doReturn;
+
+@AutoConfigureMockMvc
+@SpringBootTest
+public class UserServiceTest {
+
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private UserFactory userFactory;
+
+    @MockBean
+    private UserRepository userRepository;
+
+    @MockBean
+    private VerificationTokenRepository verificationTokenRepository;
+
+    @MockBean
+    private KeycloakService keycloakService;
+
+    @MockBean
+    private MailUtil mailUtil;
+
+    @Test
+    @DisplayName("Create user without error")
+    public void createUser(){
+        doReturn(false)
+                .when(userRepository).existsByUsername(any());
+        doReturn(false)
+                .when(userRepository).existsByEmail(any());
+        doReturn(null)
+                .when(userRepository).saveAndFlush(any());
+        doReturn(null)
+                .when(verificationTokenRepository).saveAndFlush(any());
+
+        UserResponse response = userService.createUser(SampleData.getSampleDTO());
+
+        assertThat(UserResponseType.USER_CREATED).isEqualTo(response.getType());
+    }
+
+    @Test
+    @DisplayName("Login user without error")
+    public void loginUser() {
+        doReturn(Optional.of(SampleData.getSampleUser()))
+                .when(userRepository).findByUsername(any());
+        doReturn(SampleData.getSampleDTO().getToken())
+                .when(keycloakService).getToken(any(), any());
+
+        UserResponse response = userService.loginUser(SampleData.getSampleDTO().getUsername(), SampleData.getSampleDTO().getPassword());
+
+        assertThat(response.getType()).isEqualTo(UserResponseType.USER_LOGGED_IN);
+        assertThat(response.getPayload().getToken().getToken()).isEqualTo(SampleData.getSampleDTO().getToken().getToken());
+    }
+
+}
