ce0440fb2292835ac542ae3c18b62a73a9944746
Garira MOUNDI <mazougarira@yahoo.fr>
2020-11-27 15:36:56 +0100

    deployment

--- location/src/main/java/org/projet_iwa/location/api/controller/LocationController.java
+++ location/src/main/java/org/projet_iwa/location/api/controller/LocationController.java
@@ -1,7 +1,6 @@
 package org.projet_iwa.location.api.controller;
 
 
-import org.bouncycastle.asn1.tsp.TimeStampReq;
 import org.projet_iwa.location.api.model.LocationDTO;
 import org.projet_iwa.location.api.model.Response;
 import org.projet_iwa.location.api.service.ILocationService;
@@ -9,7 +8,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.security.RolesAllowed;
-import java.sql.Timestamp;
 
 @RestController
 @CrossOrigin
@@ -24,7 +22,7 @@ public class LocationController {
 //        return iLocationService.sendLocation(locationDTO);
 //    }
 
-    @PostMapping("/")
+    @PostMapping
     @RolesAllowed("user")
     public Response<?,?> threatLocation(@RequestBody LocationDTO locationDTO){
         return iLocationService.threatLocation(locationDTO);
